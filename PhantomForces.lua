6f55d3f387d42c6f6e73c29504f87be515b6d1f5
_t38 <molecannon@outlook.com>
2024-08-02 14:57:00 +0100

    Add files via upload

---
+++ PhantomForces.lua
@@ -0,0 +1,6 @@
+-- // ESP CONFIGURATION \\ --
+_G.sex = true --> true: ESP Enabled | false: ESP Disabled (Use the switch key)
+_G.switchKey = Enum.KeyCode.RightShift --> List: https://create.roblox.com/docs/reference/engine/enums/KeyCode
+
+-- // DONT TOUCH \\ --
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdwinS7/RedactedProjectPF/main/src.lua'))()
